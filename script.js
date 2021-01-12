292436ea40f6823b0adffa81db8a5e9fd2be6847
CaeRoberts <roberts.caeron@me.com>
2021-01-12 20:55:16 +0000

    Added Pomodoro functionality
    Added Pomodoro countdown style functionality to the project.
    
    Made it a simple to use, manually operated Pomodoro
    
    **** Still need to add audio to the finish timers *****
    
    Moving onto to do list next!

--- script.js
+++ script.js
@@ -2,40 +2,50 @@
 /// Definitions//
 /////////////////
 
-let countdown;
-
-const timerSettings = {
-  study: 25,
-  short: 5,
-  long: 45,
-  intervals: 4,
-};
-
 /////////// BUTTONS ////////////////
-const startBtn = document.getElementById("startBtn");
 
-///////// FUNCTIONALITY /////////////
+const startBtn = document.getElementById("startBtn");
+const start2Btn = document.getElementById("startBtn2");
+const pauseBtn = document.getElementById("pauseBtn");
+
+/////////// SECTIONS ///////////////
 const timerDisplay = document.querySelector(".displayTimer");
 
 const titleDisplay = document.querySelector(".titleDisplay");
 
 const endTime = document.querySelector(".timeLeft");
 
+const pomoSection = document.querySelector(".Pomosection");
+
+///////////////////////////////////
+let countdown;
+
+let timerSettings = {
+  study: 25,
+  short: 5,
+  long: 45,
+  pause: 0,
+  intervals: 4,
+};
+
+///////// FUNCTIONS /////////////
+
 function timer(seconds) {
-  clearInterval(countdown);
+  // 1500
   const now = Date.now();
   const then = now + seconds * 1000;
+  clearInterval(countdown);
   displayTimeLeft(seconds);
   displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
-
     if (secondsLeft < 0) {
       clearInterval(countdown);
+      timerEnds();
       return;
     }
-
+    timerSettings.pause = secondsLeft;
     displayTimeLeft(secondsLeft);
   }, 1000);
 }
@@ -53,6 +63,7 @@ function displayTimeLeft(seconds) {
     remainderSeconds < 10 ? "0" : ""
   }${remainderSeconds} - Pomodoro Timer`;
   titleDisplay.textContent = titleDisplayer;
+  ///////////////////////////////////////
 }
 
 function displayEndTime(timestamp) {
@@ -63,11 +74,98 @@ function displayEndTime(timestamp) {
     minutes < 10 ? "0" : ""
   }${minutes}`;
 }
+////////////////////////////////////////////////////////
+////////////////// BREAK TIMERS ///////////////////////
+///////////////////////////////////////////////////////
+
+function timerBreak(seconds) {
+  // 1500
+  const now = Date.now();
+  const then1 = now + seconds * 1000;
+  clearInterval(countdown);
+  displayTimeLeft(seconds);
+  displayEndTime1(then1);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then1 - Date.now()) / 1000);
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      breakEnds();
+      return;
+    }
+    timerSettings.pause = secondsLeft;
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayEndTime1(timestamp) {
+  const end1 = new Date(timestamp);
+  const hour1 = end1.getHours();
+  const minutes1 = end1.getMinutes();
+  endTime.textContent = `Break until ${hour1 < 10 ? "0" : ""}${hour1}:${
+    minutes1 < 10 ? "0" : ""
+  }${minutes1}`;
+}
+////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+function pause() {
+  clearInterval(countdown);
+  interval = null;
+}
+
+function timerEnds() {
+  // PLAY AUDIO HERE
+  //
+  /////////////////////
+  timerDisplay.textContent = `05:00`;
+  pauseBtn.style.display = "None";
+  start2Btn.style.display = "inline";
+}
+
+function breakEnds() {
+  // PLAY AUDIO HERE
+  //
+  /////////////////////
+  timerDisplay.textContent = `25:00`;
+  pauseBtn.style.display = "None";
+  startBtn.style.display = "inline";
+}
 /////////////////////
 /// Button Events ///
 /////////////////////
 
 startBtn.addEventListener("click", function () {
   timer(timerSettings.study * 60);
-  startBtn.textContent = `Pause`;
+  startBtn.style.display = "None";
+  pauseBtn.style.display = "inline";
+  setInterval(countdown);
 });
+
+pauseBtn.addEventListener("click", function () {
+  resumeBtn.style.display = "Inline";
+  pauseBtn.style.display = "None";
+  clearInterval(countdown);
+});
+
+resumeBtn.addEventListener("click", function () {
+  pauseBtn.style.display = "inline";
+  resumeBtn.style.display = "none";
+  timer(timerSettings.pause);
+  // const newThen = Date.now + seconds * 1000;
+});
+
+startBtn2.addEventListener("click", function () {
+  timerBreak(timerSettings.short * 60);
+  start2Btn.style.display = "None";
+  pauseBtn.style.display = "inline";
+  setInterval(countdown);
+});
+
+/* 
+////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+              To-do list section
+////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+*/
